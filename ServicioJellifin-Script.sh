f2c29e62abb31f80b7cfc2304129060d4dd2f6b8
Zer0xx17 <fpbalvarosanchez@gmail.com>
2025-02-26 19:39:19 +0100

    Update ServicioJellifin-Script.sh
    Actualizacion mejorada y mostrar version Jellyfin

--- ServicioJellifin-Script.sh
+++ ServicioJellifin-Script.sh
@@ -5,7 +5,7 @@ echo "Indica una opción:"
 echo "--------------------"
 echo "1 - Instalar"
 echo "2 - Actualizar"
-echo "3 - Verificar Versión"
+echo "3 - Verificar versión"
 echo "4 - Desplegar servicio con dockers"
 read -p "Selecciona una opción (1-4): " opcion
 
@@ -35,36 +35,26 @@ case $opcion in
         echo "Instalación completada. Accede a Jellyfin en: http://localhost:8096"
         ;;
     2)
-        echo "Ejecutando actualización..."
-        echo "Actualizando el sistema..."
-        sudo apt update && sudo apt upgrade -y
+        if apt list --upgradable 2>/dev/null | grep -q "jellyfin"; then
+                echo "Jellyfin tiene una actualización disponible."
+                read -p "¿Deseas actualizar Jellifin? (S/n): " actualizar
+                if [[ -z "$actualizar" || "$actualizar" =~ ^[Ss]$ ]]; then
+                    echo "Preparando actualizacion..."
+                    echo "Deteniendo Jellyfin..."
+                    sudo systemctl stop jellyfin
+                    sudo apt install --only-upgrade jellyfin
+                    echo "Actualización completada. Accede a Jellyfin en: http://localhost:8096"
+                fi
+            else
+                echo "Jellyfin no tiene ninguna actualizacion"
+            fi
 
-        echo "Verificando repositorio de Jellyfin..."
-        if [ ! -f /usr/share/keyrings/jellyfin.asc ]; then
-        echo "Clave GPG no encontrada. Descargando de nuevo..."
-        wget -O - https://repo.jellyfin.org/debian/jellyfin_team.gpg.key | sudo tee /usr/share/keyrings/jellyfin.asc
-        fi
-
-        if [ ! -f /etc/apt/sources.list.d/jellyfin.list ]; then
-        echo "Repositorio de Jellyfin no encontrado. Agregándolo de nuevo..."
-        echo "deb [signed-by=/usr/share/keyrings/jellyfin.asc] https://repo.jellyfin.org/debian $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
-        fi
-
-        echo "Actualizando Jellyfin a la última versión disponible..."
-        sudo apt update && sudo apt upgrade -y jellyfin
-
-        echo "Reiniciando el servicio Jellyfin..."
-        sudo systemctl restart jellyfin
-
-        echo "Estado del servicio Jellyfin:"
-        sudo systemctl status jellyfin --no-pager
-
-        echo "Actualización completada. Accede a Jellyfin en: http://localhost:8096"
+        
 
         ;;
     3)
-        echo "Verificando versión..."
-        
+        echo "La version actual de Jellyfin es:" 
+        jellyfin --version
         ;;
 
     4)
@@ -89,8 +79,8 @@ else
         sudo apt install docker-ce
         echo "Docker instalado correctamente."
         docker --version
-        fi
-        fi
+    fi
+fi
       ;;  
 
       
